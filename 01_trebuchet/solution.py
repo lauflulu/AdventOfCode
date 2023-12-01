395637feabf69f28cc8508dcee5e4de27c0bee94
laufinger <l93aufinger@gmail.com>
2023-12-01 20:29:30 +0100

    feat: Compute result for example data 2

--- 01_trebuchet/solution.py
+++ 01_trebuchet/solution.py
@@ -1,13 +1,35 @@
 import re
 
 
+map_literal_digits = {
+    'zero': '0',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
+
+
 def read_data(filename: str) -> list[str]:
     with open(filename, 'r') as file:
         return [line.strip() for line in file]
 
 
-def _find_all_digits(line: str):
-    return re.findall(r'[0-9]', line)
+def _find_all_digits(line: str) -> list[str]:
+    matches = re.finditer(r'(?=([0-9]|zero|one|two|three|four|five|six|seven|eight|nine))', line)
+    literal_digits = [match.group(1) for match in matches]
+    int_digits = []
+    for digit in literal_digits:
+        if digit in map_literal_digits.keys():
+            int_digits.append(map_literal_digits[digit])
+        else:
+            int_digits.append(digit)
+    return int_digits
 
 
 def _extract_two_digits(line: str):
