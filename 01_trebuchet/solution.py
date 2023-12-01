b0b3261109cd38b58a9d19d6c27bd7a3b7234f2e
laufinger <l93aufinger@gmail.com>
2023-12-01 19:52:02 +0100

    feat: Compute the final answer

--- 01_trebuchet/solution.py
+++ 01_trebuchet/solution.py
@@ -18,3 +18,12 @@ def extract_two_digits(code_line: str):
 def answer(lines):
     two_digits = [extract_two_digits(line) for line in lines]
     return sum(two_digits)
+
+
+def main():
+    lines = read_data("material.txt")
+    print(answer(lines))
+
+
+if __name__ == '__main__':
+    main()
