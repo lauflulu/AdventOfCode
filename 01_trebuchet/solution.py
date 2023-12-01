db04dd5124896e5e428311abe8a45a38ce7eefa4
laufinger <l93aufinger@gmail.com>
2023-12-01 19:39:10 +0100

    test: Ensure the final answer is correct for example data

--- 01_trebuchet/solution.py
+++ 01_trebuchet/solution.py
@@ -8,3 +8,9 @@ def _find_all_digits(code_line: str):
 def extract_two_digits(code_line: str):
     all_digits = _find_all_digits(code_line)
     return int(all_digits[0] + all_digits[-1])
+
+
+def answer(puzzle_text):
+    lines = puzzle_text.split("\n")
+    two_digits = [extract_two_digits(line) for line in lines]
+    return sum(two_digits)
--- 01_trebuchet/test.py
+++ 01_trebuchet/test.py
@@ -3,6 +3,9 @@ import pytest
 import solution
 
 
+EXAMPLE_DATA = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"
+
+
 @pytest.mark.parametrize("code_line, expected_result", [
     ("1abc2", 12),
     ("pqr3stu8vwx", 38),
@@ -12,3 +15,7 @@ import solution
 def test_that_first_and_last_digits_are_extracted(code_line, expected_result):
     result = solution.extract_two_digits(code_line)
     assert result == expected_result
+
+
+def test_that_the_correct_sum_is_computed_for_example_data():
+    assert solution.answer(EXAMPLE_DATA) == 142
