edf7361ea0cee43018a86e25919f0d5cf11f35f6
laufinger <l93aufinger@gmail.com>
2023-12-01 19:56:36 +0100

    refactor: Add example data for part two

--- 01_trebuchet/test.py
+++ 01_trebuchet/test.py
@@ -3,21 +3,24 @@ import pytest
 import solution
 
 
-def test_that_read_example_data_is_the_correct_list():
-    assert solution.read_data("example_data.txt") == ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+class TestPartOne:
+    def test_that_read_example_data_is_the_correct_list(self):
+        assert solution.read_data("example_data.txt") == ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+
+    @pytest.mark.parametrize("code_line, expected_result", [
+        ("1abc2", 12),
+        ("pqr3stu8vwx", 38),
+        ("a1b2c3d4e5f", 15),
+        ("treb7uchet", 77),
+    ])
+    def test_that_first_and_last_digits_are_extracted(self, code_line, expected_result):
+        result = solution._extract_two_digits(code_line)
+        assert result == expected_result
+
+    def test_that_the_correct_sum_is_computed_for_example_data(self):
+        example_data = solution.read_data("example_data.txt")
+        assert solution.compute_total_sum(example_data) == 142
 
 
-@pytest.mark.parametrize("code_line, expected_result", [
-    ("1abc2", 12),
-    ("pqr3stu8vwx", 38),
-    ("a1b2c3d4e5f", 15),
-    ("treb7uchet", 77),
-])
-def test_that_first_and_last_digits_are_extracted(code_line, expected_result):
-    result = solution._extract_two_digits(code_line)
-    assert result == expected_result
-
-
-def test_that_the_correct_sum_is_computed_for_example_data():
-    example_data = solution.read_data("example_data.txt")
-    assert solution.compute_total_sum(example_data) == 142
+class TestPartTwo:
+    pass
