4bdb062e5c72bcc779189a659585918458bf71df
laufinger <l93aufinger@gmail.com>
2023-12-01 19:51:00 +0100

    test: Ensure the puzzle data is read correctly

--- 01_trebuchet/solution.py
+++ 01_trebuchet/solution.py
@@ -1,6 +1,11 @@
 import re
 
 
+def read_data(filename: str) -> list[str]:
+    with open(filename, 'r') as file:
+        return [line.strip() for line in file]
+
+
 def _find_all_digits(code_line: str):
     return re.findall(r'[0-9]', code_line)
 
@@ -10,7 +15,6 @@ def extract_two_digits(code_line: str):
     return int(all_digits[0] + all_digits[-1])
 
 
-def answer(puzzle_text):
-    lines = puzzle_text.split("\n")
+def answer(lines):
     two_digits = [extract_two_digits(line) for line in lines]
     return sum(two_digits)
--- 01_trebuchet/test.py
+++ 01_trebuchet/test.py
@@ -3,7 +3,8 @@ import pytest
 import solution
 
 
-EXAMPLE_DATA = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"
+def test_that_read_example_data_is_the_correct_list():
+    assert solution.read_data("example_data.txt") == ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
 
 
 @pytest.mark.parametrize("code_line, expected_result", [
@@ -18,4 +19,5 @@ def test_that_first_and_last_digits_are_extracted(code_line, expected_result):
 
 
 def test_that_the_correct_sum_is_computed_for_example_data():
-    assert solution.answer(EXAMPLE_DATA) == 142
+    example_data = solution.read_data("example_data.txt")
+    assert solution.answer(example_data) == 142
