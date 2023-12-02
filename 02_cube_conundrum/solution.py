dd8084feb437830361fb5d3682c1cbc08827684b
laufinger <l93aufinger@gmail.com>
2023-12-02 10:16:04 +0100

    feat: Load data into structure

--- 02_cube_conundrum/solution.py
+++ 02_cube_conundrum/solution.py
@@ -1,5 +1,23 @@
 # 12 red cubes, 13 green cubes, and 14 blue cubes.
+import re
 
 
 def load_data(filename: str):
-    pass
+    with open(filename, 'r') as file:
+        return [_parse_line(line) for line in file]
+
+
+def _parse_line(line: str) -> list[dict]:
+    draw_strings = line.split(':')[1].split(';')
+    draw_dicts = [_parse_rgb(draw_string) for draw_string in draw_strings]
+    return draw_dicts
+
+
+def _parse_rgb(rgb_string: str) -> dict:
+    rgbs = rgb_string.split(',')
+    rgb_dict = {'red': 0, 'green': 0, 'blue': 0}
+    for rgb in rgbs:
+        for color in rgb_dict.keys():
+            if color in rgb:
+                rgb_dict[color] = int(re.findall(r'[0-9]', rgb)[0])
+    return rgb_dict
