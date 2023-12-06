efcc41dc4ac90d784c88a0609c7a5952978137c8
laufinger <l93aufinger@gmail.com>
2023-12-06 20:42:39 +0100

    feat: Load data

--- 04_scratchcards/solution.py
+++ 04_scratchcards/solution.py
@@ -1,15 +1,24 @@
 class Card:
 
-    def __init__(self, winning_numbers, scratch_numbers):
-        self.winning_numbers: tuple = winning_numbers
-        self.scratch_numbers: tuple = scratch_numbers
+    def __init__(self, winning_numbers: tuple[int], scratch_numbers: tuple[int]):
+        self.winning_numbers: tuple[int] = winning_numbers
+        self.scratch_numbers: tuple[int] = scratch_numbers
 
     def value(self):
         pass
 
 
-def load_data(filepath) -> list[Card]:
-    pass
+def load_data(filepath: str) -> list[Card]:
+    with open(filepath, 'r') as file:
+        return [_parse(line) for line in file]
+
+
+def _parse(line: str):
+    _, numbers = line.split(':')
+    winning_numbers, scratch_numbers = numbers.split('|')
+    winning_numbers = tuple([int(number.strip()) for number in winning_numbers.split(' ') if not number == ''])
+    scratch_numbers = tuple([int(number.strip()) for number in scratch_numbers.split(' ') if not number == ''])
+    return Card(winning_numbers, scratch_numbers)
 
 
 def get_result(data) -> int:
