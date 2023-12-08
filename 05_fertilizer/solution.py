45ea55e3ea1096877162dae717953b983b20f1d7
laufinger <l93aufinger@gmail.com>
2023-12-08 20:30:50 +0100

    feat: Load data

--- 05_fertilizer/solution.py
+++ 05_fertilizer/solution.py
@@ -1,9 +1,12 @@
+import re
+
+
 class Map:
 
-    def __init__(self):
-        self.source = ''
-        self.destination = ''
-        self.ranges: list[Range] = []
+    def __init__(self, source, destination, ranges):
+        self.source = source
+        self.destination = destination
+        self.ranges: list[Range] = ranges
 
     def map(self, source_number):
         for range_ in self.ranges:
@@ -14,10 +17,10 @@ class Map:
 
 
 class Range:
-    def __init__(self):
-        self.source_start = 0
-        self.destination_start = 0
-        self.length = 0
+    def __init__(self, destination_start: int, source_start: int, length: int):
+        self.source_start = source_start
+        self.destination_start = destination_start
+        self.length = length
 
     def destination(self, source_number) -> int | None:
         if self.source_start <= source_number <= self.source_start + self.length:
@@ -25,7 +28,26 @@ class Range:
 
 
 def load_data(filename) -> tuple[list[int], list[Map]]:
-    pass
+    maps = []
+    with open(filename, 'r') as file:
+        seeds = [int(seed.strip()) for seed in file.readline().split(' ')[1:]]
+
+        lines = file.readlines()[1:]
+        source = ''
+        destination = ''
+        ranges = []
+        for line in lines:
+            if "map" in line:
+                source, _, destination = line.split(' ')[0].split('-')
+            elif re.match(r'[0-9]+ [0-9]+ [0-9]+', line):
+                ranges.append(Range(*[int(n.strip()) for n in line.split(' ')]))
+            else:
+                maps.append(Map(source, destination, ranges))
+                source = ''
+                destination = ''
+                ranges = []
+        maps.append(Map(source, destination, ranges))
+    return seeds, maps
 
 
 def map_categories(number, source, destination):
