ec5683acd1448ae07763402c2769a86f546f204c
laufinger <l93aufinger@gmail.com>
2023-12-14 21:14:12 +0100

    feat: Find start position

--- 10_pipe_maze/solution.py
+++ 10_pipe_maze/solution.py
@@ -5,12 +5,14 @@ class Maze:
 
     def __init__(self, tiles: np.array):
         self._tiles = tiles
-        self._current_x = 0
-        self._current_y = 0
+        self._current_y, self._current_x = self._start_index()
         self._last_direction = None
 
     def solve(self) -> int:
         pass
+
+    def _start_index(self):
+        return np.argwhere(self._tiles == 'S')[0]
 
 
 def load_data(filename: str) -> Maze:
