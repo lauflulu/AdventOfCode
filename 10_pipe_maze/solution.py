5b7a73183bc9f8114e18b88648e4dec020c94d13
laufinger <l93aufinger@gmail.com>
2023-12-14 21:52:19 +0100

    feat: Compute result 1

--- 10_pipe_maze/solution.py
+++ 10_pipe_maze/solution.py
@@ -9,7 +9,42 @@ class Maze:
         self._last_direction = None
 
     def solve(self) -> int:
-        pass
+        c = 0
+        self.move()
+        while not all((self._current_y, self._current_x) == self._start_index()):
+            c += 1
+            self.move()
+        return int(c/2 + 1)
+
+    def move(self):
+        for _y, _x in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            if self._last_direction == (-_y, -_x):
+                continue
+            if not 0 <= self._current_y + _y < self._tiles.shape[0]:
+                continue
+            if not 0 <= self._current_x + _x < self._tiles.shape[1]:
+                continue
+            if (_y == 1 and self._tiles[self._current_y, self._current_x] in ['|', 'F', '7', 'S']
+                    and self._tiles[self._current_y + _y, self._current_x] in ['|', 'J', 'L', 'S']):
+                self._current_y += _y
+                self._last_direction = (_y, 0)
+                return
+            if (_x == 1 and self._tiles[self._current_y, self._current_x] in ['-', 'F', 'L', 'S']
+                    and self._tiles[self._current_y, self._current_x + _x] in ['-', 'J', '7', 'S']):
+                self._current_x += _x
+                self._last_direction = (0, _x)
+                return
+            if (_y == -1 and self._tiles[self._current_y, self._current_x] in ['|', 'L', 'J', 'S']
+                    and self._tiles[self._current_y + _y, self._current_x] in ['|', '7', 'F', 'S']):
+                self._current_y += _y
+                self._last_direction = (_y, 0)
+                return
+            if (_x == -1 and self._tiles[self._current_y, self._current_x] in ['-', 'J', '7', 'S']
+                    and self._tiles[self._current_y, self._current_x + _x] in ['-', 'F', 'L', 'S']):
+                self._current_x += _x
+                self._last_direction = (0, _x)
+                return
+
 
     def _start_index(self):
         return np.argwhere(self._tiles == 'S')[0]
