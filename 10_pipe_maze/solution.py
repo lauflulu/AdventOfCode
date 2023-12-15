4154bc6b2db1f2581fc66ea567418d0ff6f56bfc
laufinger <l93aufinger@gmail.com>
2023-12-15 20:35:46 +0100

    refactor: Extract method to get entry of current tile

--- 10_pipe_maze/solution.py
+++ 10_pipe_maze/solution.py
@@ -24,26 +24,29 @@ class Maze:
                 continue
             if not 0 <= self._current_x + _x < self._tiles.shape[1]:
                 continue
-            if (_y == 1 and self._tiles[self._current_y, self._current_x] in ['|', 'F', '7', 'S']
+            if (_y == 1 and self._current_tile() in ['|', 'F', '7', 'S']
                     and self._tiles[self._current_y + _y, self._current_x] in ['|', 'J', 'L', 'S']):
                 self._current_y += _y
                 self._last_direction = (_y, 0)
                 return
-            if (_x == 1 and self._tiles[self._current_y, self._current_x] in ['-', 'F', 'L', 'S']
+            if (_x == 1 and self._current_tile() in ['-', 'F', 'L', 'S']
                     and self._tiles[self._current_y, self._current_x + _x] in ['-', 'J', '7', 'S']):
                 self._current_x += _x
                 self._last_direction = (0, _x)
                 return
-            if (_y == -1 and self._tiles[self._current_y, self._current_x] in ['|', 'L', 'J', 'S']
+            if (_y == -1 and self._current_tile() in ['|', 'L', 'J', 'S']
                     and self._tiles[self._current_y + _y, self._current_x] in ['|', '7', 'F', 'S']):
                 self._current_y += _y
                 self._last_direction = (_y, 0)
                 return
-            if (_x == -1 and self._tiles[self._current_y, self._current_x] in ['-', 'J', '7', 'S']
+            if (_x == -1 and self._current_tile() in ['-', 'J', '7', 'S']
                     and self._tiles[self._current_y, self._current_x + _x] in ['-', 'F', 'L', 'S']):
                 self._current_x += _x
                 self._last_direction = (0, _x)
                 return
+
+    def _current_tile(self) -> str:
+        return self._tiles[self._current_y, self._current_x]
 
     def _start_index(self):
         return np.argwhere(self._tiles == 'S')[0]
