c64fb91b7328f486e31605844b39d87c9cbc95e2
laufinger <l93aufinger@gmail.com>
2023-12-15 20:31:11 +0100

    refactor: Rename method

--- 10_pipe_maze/solution.py
+++ 10_pipe_maze/solution.py
@@ -8,13 +8,13 @@ class Maze:
         self._current_y, self._current_x = self._start_index()
         self._last_direction = None
 
-    def solve(self) -> int:
+    def count_loop_tiles(self) -> int:
         c = 0
         self.move()
         while not all((self._current_y, self._current_x) == self._start_index()):
             c += 1
             self.move()
-        return int(c/2 + 1)
+        return c
 
     def move(self):
         for _y, _x in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
@@ -45,7 +45,6 @@ class Maze:
                 self._last_direction = (0, _x)
                 return
 
-
     def _start_index(self):
         return np.argwhere(self._tiles == 'S')[0]
 
@@ -59,7 +58,7 @@ def load_data(filename: str) -> Maze:
 
 
 def get_result(maze):
-    return maze.solve()
+    return int(maze.count_loop_tiles()/2 + 1)
 
 
 def get_result_2(data):
