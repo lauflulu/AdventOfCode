a611a3e2723d694a00aedb6a73ffa97be1c95000
laufinger <l93aufinger@gmail.com>
2023-12-15 21:18:36 +0100

    refactor: Extract method to check start position

--- 10_pipe_maze/solution.py
+++ 10_pipe_maze/solution.py
@@ -20,23 +20,8 @@ class Maze:
 
     def move(self):
         if self._current_tile() == 'S':
-            for _y, _x in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                if not 0 <= self._current_y + _y < self._tiles.shape[0]:
-                    continue
-                if not 0 <= self._current_x + _x < self._tiles.shape[1]:
-                    continue
-                if _y == 1 and self._neighboring_tile(_y, _x) in ['|', 'J', 'L', 'S']:
-                    self._update_position(_y, _x)
-                    return
-                if _x == 1 and self._neighboring_tile(_y, _x) in ['-', 'J', '7', 'S']:
-                    self._update_position(_y, _x)
-                    return
-                if _y == -1 and self._neighboring_tile(_y, _x) in ['|', '7', 'F', 'S']:
-                    self._update_position(_y, _x)
-                    return
-                if _x == -1 and self._neighboring_tile(_y, _x) in ['-', 'F', 'L', 'S']:
-                    self._update_position(_y, _x)
-                    return
+            self._check_start_position()
+            return
 
         for _y, _x in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
             if self._last_direction == (-_y, -_x):
@@ -75,6 +60,25 @@ class Maze:
 
     def _start_index(self):
         return np.argwhere(self._tiles == 'S')[0]
+
+    def _check_start_position(self):
+        for _y, _x in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            if not 0 <= self._current_y + _y < self._tiles.shape[0]:
+                continue
+            if not 0 <= self._current_x + _x < self._tiles.shape[1]:
+                continue
+            if _y == 1 and self._neighboring_tile(_y, _x) in ['|', 'J', 'L']:
+                self._update_position(_y, _x)
+                return
+            if _x == 1 and self._neighboring_tile(_y, _x) in ['-', 'J', '7']:
+                self._update_position(_y, _x)
+                return
+            if _y == -1 and self._neighboring_tile(_y, _x) in ['|', '7', 'F']:
+                self._update_position(_y, _x)
+                return
+            if _x == -1 and self._neighboring_tile(_y, _x) in ['-', 'F', 'L']:
+                self._update_position(_y, _x)
+                return
 
 
 def load_data(filename: str) -> Maze:
