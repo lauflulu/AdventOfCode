686edc4f8abaee7efab5b81c887e43b3d7637646
laufinger <l93aufinger@gmail.com>
2023-12-17 12:59:25 +0100

    feat: Compute result for part 2 with small expansion factors

--- 11_cosmic_expansion/solution.py
+++ 11_cosmic_expansion/solution.py
@@ -4,20 +4,22 @@ import numpy as np
 class Universe:
     def __init__(self, map_of_the_universe: np.array):
         self._map = map_of_the_universe
+        self._expansion_factor = 2
 
     def _expand(self):
         expanded_rows = []
         for row in range(self._map.shape[0]):
             expanded_rows.append(self._map[row, :])
             if np.all(self._map[row, :] == '.'):
-                expanded_rows.append(self._map[row, :])
+                for _ in range(self._expansion_factor-1):
+                    expanded_rows.append(self._map[row, :])
         expanded_rows = np.array(expanded_rows)
         expanded_cols = []
         for col in range(expanded_rows.shape[1]):
             expanded_cols.append(expanded_rows[:, col])
             if np.all(expanded_rows[:, col] == '.'):
-                expanded_cols.append(expanded_rows[:, col])
-
+                for _ in range(self._expansion_factor-1):
+                    expanded_cols.append(expanded_rows[:, col])
         return np.array(expanded_cols).T
 
     def _galaxy_coordinates(self) -> list[tuple]:
@@ -32,7 +34,8 @@ class Universe:
                 paths.append(distance)
         return paths
 
-    def total_distance(self) -> int:
+    def total_distance(self, expansion_factor: int = 2) -> int:
+        self._expansion_factor = expansion_factor
         return sum(self._galaxy_distances())
 
 
@@ -48,14 +51,14 @@ def get_result(universe):
     return universe.total_distance()
 
 
-def get_result_2(data):
-    pass
+def get_result_2(universe, expansion_factor):
+    return universe.total_distance(expansion_factor)
 
 
 def main():
     data = load_data("data.txt")
     print(get_result(data))
-    print(get_result_2(data))
+    print(get_result_2(data, 1_000_000))
 
 
 if __name__ == '__main__':
--- 11_cosmic_expansion/test.py
+++ 11_cosmic_expansion/test.py
@@ -34,4 +34,4 @@ class TestPart1:
 class TestPart2:
     @pytest.mark.parametrize("expansion_factor, result", [(10, 1030), (100, 8410)])
     def test_that_the_result_is_correct_for_example_with_expansion_factor(self, expansion_factor, result, universe):
-        assert solution.get_result_2(universe) == result
+        assert solution.get_result_2(universe, expansion_factor) == result
