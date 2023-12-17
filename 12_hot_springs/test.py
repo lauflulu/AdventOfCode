84b0b338e021dfa578a641f326fdc3a253a0f0dc
laufinger <l93aufinger@gmail.com>
2023-12-17 17:27:10 +0100

    test: Ensure potential fits are found

--- 12_hot_springs/test.py
+++ 12_hot_springs/test.py
@@ -8,8 +8,22 @@ class TestPart1:
     def test_load_data_returns_list_of_records(self):
         records = solution.load_data("example.txt")
         assert isinstance(records[0], Record)
-        assert records[0]._springs == "???.###"
+        assert records[0]._springs == ".???.###."
         assert records[2]._groups == [1, 3, 1, 6]
+
+    def test_matches(self):
+        record = Record(".??..??...?##.", [1, 1, 3])
+        assert record._matches(1, 0) is False
+        assert record._matches(1, 1) is True
+        assert record._matches(1, 2) is True
+        assert record._matches(1, 3) is False
+        assert record._matches(1, 10) is False
+        assert record._matches(3, 10) is True
+        assert record._matches(3, 1) is False
+
+    def test_fit_indices(self):
+        record = Record(".??..??...?##.", [1, 1, 3])
+        assert record._fit_indices(1) == [1, 2, 5, 6]
 
     @pytest.mark.parametrize("row_index, number_of_arrangements", enumerate([1, 4, 1, 1, 4, 10]))
     def test_that_correct_number_of_combinations_is_computed_for_each_row(self, row_index, number_of_arrangements):
