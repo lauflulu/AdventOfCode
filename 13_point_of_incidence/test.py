f56d3adee300f36febcc683fc81aa40bb01e886b
laufinger <l93aufinger@gmail.com>
2024-01-01 19:07:59 +0100

    test: Ensure part 2 is computed correctly

--- 13_point_of_incidence/test.py
+++ 13_point_of_incidence/test.py
@@ -20,3 +20,17 @@ class TestPart1:
     def test_that_result_is_correct_for_example_data(self):
         data = solution.load_data("example.txt")
         assert solution.get_result(data) == 405
+
+
+class TestPart2:
+    @pytest.mark.parametrize("terrain_index, mirror_indices", [
+        (0, (3, 0)),
+        (1, (1, 0)),
+    ])
+    def test_that_correct_mirror_indices_are_found_for_example_data(self, terrain_index, mirror_indices):
+        data = solution.load_data("example.txt")
+        assert data[terrain_index].smudge_mirror_indices() == mirror_indices
+
+    def test_that_result_is_correct_for_example_data(self):
+        data = solution.load_data("example.txt")
+        assert solution.get_result_2(data) == 400
