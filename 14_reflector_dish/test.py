a17c275cb6a179d49e44c9e371449b3c985d0e52
laufinger <l93aufinger@gmail.com>
2024-01-01 19:49:12 +0100

    test: Ensure result for part 1 is correct

---
+++ 14_reflector_dish/test.py
@@ -0,0 +1,19 @@
+import solution
+
+
+class TestPart1:
+    def test_that_data_is_loaded_as_list_of_strings_column_wise(self):
+        data = solution.load_data("example.txt").platform
+        assert data[0] == "##..O.O.OO"
+        assert len(data) == 10
+        assert len((data[0])) == 10
+
+    def test_that_data_is_tilted_correctly(self):
+        data = solution.load_data("example.txt")
+        tilted_data = solution.load_data("example_tilted.txt")
+        for d, t in zip(data, tilted_data):
+            assert d == t
+
+    def test_that_result_is_correct_for_example(self):
+        data = solution.load_data("example.txt")
+        assert solution.get_result(data) == 136
