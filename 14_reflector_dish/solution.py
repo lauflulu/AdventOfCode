fc096b7544af4b900f4b05ec7ef10d85073f42eb
laufinger <l93aufinger@gmail.com>
2024-01-01 20:14:46 +0100

    feat: Load data

--- 14_reflector_dish/solution.py
+++ 14_reflector_dish/solution.py
@@ -1,5 +1,26 @@
+
+class Platform:
+    def __init__(self, platform):
+        self.platform = platform
+        self.rotate_right()
+
+    def rotate_right(self):
+        n_rows = len(self.platform)
+        n_cols = len(self.platform[0])
+        rotated_platform = ['' for _ in range(n_rows)]
+        for r in reversed(range(n_rows)):
+            for c in range(n_cols):
+                rotated_platform[c] += self.platform[r][c]
+        self.platform = rotated_platform
+
+    def tilt(self):
+        pass
+
+
+
 def load_data(filename):
-    pass
+    with open(filename, 'r') as file:
+        return Platform([row.strip() for row in file])
 
 
 def get_result(data):
