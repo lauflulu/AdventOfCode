0d6c2f8fc35e15a54148333017a56779f1cf3ac6
laufinger <l93aufinger@gmail.com>
2024-01-02 21:13:54 +0100

    test: Ensure beams mark tiles in contraption

--- 16_floor_is_lava/test.py
+++ 16_floor_is_lava/test.py
@@ -15,5 +15,11 @@ class TestPart1:
     def test_that_beam_enters_from_one_direction(self, beam):
         assert len(beam.beam_tips) == 1
 
+    def test_that_beam_marks_tile_with_its_direction_when_passing(self):
+        beam = solution.load_data("example_3x3_empty.txt")
+        beam.beam_tips = [(1, 1, ">")]
+        beam._process_tips()
+        assert beam.contraption[1, 1] == "." + ">"
+
     def test_that_energy_is_computed(self, beam):
         assert solution.get_result(beam) == 46
