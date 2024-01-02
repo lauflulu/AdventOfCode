6c12ac4db24dcd30cc3ffc7bc63e756d253772a6
laufinger <l93aufinger@gmail.com>
2024-01-02 21:06:40 +0100

    refactor: Use fixture for beam

--- 16_floor_is_lava/test.py
+++ 16_floor_is_lava/test.py
@@ -1,11 +1,16 @@
+import pytest
+
 import solution
 
 
+@pytest.fixture
+def beam():
+    return solution.load_data("example.txt")
+
+
 class TestPart1:
-    def test_that_data_is_loaded(self):
-        beam = solution.load_data("example.txt")
+    def test_that_data_is_loaded(self, beam):
         assert beam.contraption.shape == (10, 10)
 
-    def test_that_energy_is_computed(self):
-        beam = solution.load_data("example.txt")
+    def test_that_energy_is_computed(self, beam):
         assert solution.get_result(beam) == 46
