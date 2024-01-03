3165e41cba7bde0b1a6329edcd1bafca4e181366
laufinger <l93aufinger@gmail.com>
2024-01-03 19:53:02 +0100

    refactor: Initialize day 17

---
+++ 17_clumsy_crucible/test.py
@@ -0,0 +1,14 @@
+import solution
+
+
+class TestPart1:
+    def test_that_data_is_loaded(self):
+        pass
+
+    def test_that_result_is_correct_for_example(self):
+        assert solution.get_result() == 0
+
+
+class TestPart2:
+    def test_that_result_is_correct_for_example(self):
+        assert solution.get_result_2() == 0
