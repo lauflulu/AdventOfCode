c507514b1ca67f60c10cb9c4f5984ddca98e76b8
laufinger <l93aufinger@gmail.com>
2024-01-03 13:47:04 +0100

    test: Ensure beam is reflected by mirrors

--- 16_floor_is_lava/test.py
+++ 16_floor_is_lava/test.py
@@ -27,5 +27,20 @@ class TestPart1:
         beam.process_tips()
         assert beam.tips[0] == (1, 2, ">")
 
+    @pytest.mark.parametrize("in_tip, out_tip", [
+        ((1, 4, ">"), (2, 4, "v")),
+        ((1, 4, "<"), (0, 4, "^")),
+        ((1, 4, "^"), (1, 3, "<")),
+        ((1, 4, "v"), (1, 5, ">")),
+        ((6, 4, ">"), (5, 4, "^")),
+        ((6, 4, "<"), (7, 4, "v")),
+        ((6, 4, "^"), (6, 5, ">")),
+        ((6, 4, "v"), (6, 3, "<")),
+    ])
+    def test_that_mirrors_reflect_direction(self, beam, in_tip, out_tip):
+        beam.tips = [in_tip]
+        beam.process_tips()
+        assert beam.tips[0] == out_tip
+
     def test_that_energy_is_computed(self, beam):
         assert solution.get_result(beam) == 46
