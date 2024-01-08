f126e753a41014a707dfd0c7b4ce2f1118f75890
laufinger <l93aufinger@gmail.com>
2024-01-08 19:54:56 +0100

    test: Ensure instructions are loaded

--- 18_lavaduct_lagoon/solution.py
+++ 18_lavaduct_lagoon/solution.py
@@ -1,3 +1,29 @@
+class Instruction:
+    def __init__(self, line: str):
+        self.direction, self.distance, self.color = self._parse(line)
+
+    def _parse(self, line: str) -> tuple[str, int, str]:
+        pass
+
+
+class Lagoon:
+    def __init__(self, instructions: list[Instruction]):
+        self._instructions = instructions
+        self.y, self.x = (0, 0)
+
+    def get_result(self) -> int:
+        pass
+
+    def precalculate_shape(self) -> tuple[int, int]:
+        pass
+
+    def dig_outline(self):
+        pass
+
+    def volume(self) -> int:
+        pass
+
+
 def load_data(filename):
     pass
 
--- 18_lavaduct_lagoon/test.py
+++ 18_lavaduct_lagoon/test.py
@@ -1,15 +1,41 @@
+import pytest
+
 import solution
 
 
-class TestPart1:
-    def test_that_data_is_loaded(self):
-        pass
+@pytest.fixture
+def lagoon():
+    instructions = solution.load_data("example.txt")
+    return solution.Lagoon(instructions)
 
+
+class TestPart1:
+    def test_that_instructions_are_loaded(self):
+        instructions = solution.load_data("example.txt")
+        assert instructions[0].direction == (0, 1)
+        assert instructions[0].distance == 6
+        assert instructions[0].color == '#70c710'
+
+    @pytest.mark.skip
+    def test_that_shape_can_be_pre_calculated(self, lagoon):
+        assert lagoon.precalculate_shape() == (10, 7)
+
+    @pytest.mark.skip
+    def test_that_the_outline_ends_where_it_starts(self, lagoon):
+        lagoon.dig_outline()
+        assert lagoon.y, lagoon.x == (0, 0)
+
+    @pytest.mark.skip
+    def test_that_the_outline_has_correct_volume(self, lagoon):
+        lagoon.dig_outline()
+        assert lagoon.volume() == 38
+
+    @pytest.mark.skip
     def test_that_result_is_correct_for_example(self):
         data = solution.load_data("example.txt")
-        assert solution.get_result(data) == 0
+        assert solution.get_result(data) == 62
 
-
+@pytest.mark.skip
 class TestPart2:
     def test_that_result_is_correct_for_example(self):
         data = solution.load_data("example.txt")
