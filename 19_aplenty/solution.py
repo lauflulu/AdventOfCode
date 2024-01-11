55f56c76cf5e33def0a276936d98022acb7a5d93
laufinger <l93aufinger@gmail.com>
2024-01-11 21:32:39 +0100

    feat: Load data with correct types

--- 19_aplenty/solution.py
+++ 19_aplenty/solution.py
@@ -1,13 +1,28 @@
 class Part:
-    pass
+    def __init__(self, line):
+        pass
 
 
 class Workflow:
-    pass
+    def __init__(self, line):
+        pass
 
 
 def load_data(filename):
-    pass
+    workflows = {}
+    parts = []
+    workflows_read = False
+    with open(filename, "r") as file:
+        for line in file:
+            if line == "\n":
+                workflows_read = True
+            if not workflows_read:
+                name, rules = line.strip().split("{")
+                workflows[name] = Workflow(rules[:-1])
+            else:
+                line = file.readline()
+                parts.append(Part(line[1:-1]))
+    return workflows, parts
 
 
 def get_result(data):
