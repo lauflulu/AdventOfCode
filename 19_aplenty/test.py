566620b96c946c59014427ec3008740bcf58e66d
laufinger <l93aufinger@gmail.com>
2024-01-11 21:40:48 +0100

    test: Ensure parts have correct ratings

--- 19_aplenty/test.py
+++ 19_aplenty/test.py
@@ -8,6 +8,10 @@ class TestPart1:
         _, parts = solution.load_data("example.txt")
         assert isinstance(parts[0], solution.Part)
 
+    def test_that_parts_are_loaded_with_correct_ratings(self):
+        _, parts = solution.load_data("example.txt")
+        assert parts[0].ratings == {"x": 787, "m": 2655, "a": 1222, "s": 2876}
+
     def test_that_loaded_data_contains_dict_of_workflows(self):
         workflows, _ = solution.load_data("example.txt")
         assert isinstance(workflows["in"], solution.Workflow)
