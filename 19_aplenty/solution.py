e8f1a14930daeb477f3e18659c8910a3e901ee49
laufinger <l93aufinger@gmail.com>
2024-01-11 21:19:31 +0100

    refactor: Initialize day 19

---
+++ 19_aplenty/solution.py
@@ -0,0 +1,20 @@
+def load_data(filename):
+    pass
+
+
+def get_result(data):
+    pass
+
+
+def get_result_2(data):
+    pass
+
+
+def main():
+    data = load_data("data.txt")
+    print(get_result(data))
+    print(get_result_2(data))
+
+
+if __name__ == '__main__':
+    main()
