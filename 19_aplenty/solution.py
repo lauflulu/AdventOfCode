0bb3d773cd581aafaa02ce3b3e082ca8dd3b94d7
laufinger <l93aufinger@gmail.com>
2024-01-14 20:53:35 +0100

    refactor: Prototype part 2

--- 19_aplenty/solution.py
+++ 19_aplenty/solution.py
@@ -19,6 +19,18 @@ class Part:
         return sum(self.ratings.values())
 
 
+class PartRange:
+    def __init__(self, workflow_id, xmas: dict[str, tuple[int, int]]):
+        self.workflow_id = workflow_id
+        self.xmas = xmas
+
+    def combinations(self):
+        product = 1
+        for x in self.xmas.values():
+            product *= (x[1] - x[0])
+        return product
+
+
 class Rule:
     def __init__(self, rule_string):
         self._condition , self._return = self._parse(rule_string)
@@ -43,6 +55,24 @@ class Rule:
                 return self._return
         return ""
 
+    def split(self, part_range: PartRange) -> tuple[PartRange, PartRange]:
+        category, operator, number  = self._condition
+        if operator == ">":
+            xmas = part_range.xmas
+            xmas[category] = (number + 1, xmas[category][1])
+            accepted_range = PartRange(self._return, xmas)
+            xmas = part_range.xmas
+            xmas[category] = (xmas[category][0], number)
+            rejected_range = PartRange("", xmas)
+        else:
+            xmas = part_range.xmas
+            xmas[category] = (xmas[category][0], number - 1)
+            accepted_range = PartRange(self._return, xmas)
+            xmas = part_range.xmas
+            xmas[category] = (number, xmas[category][1])
+            rejected_range = PartRange("", xmas)
+        return accepted_range, rejected_range
+
 
 class Workflow:
     def __init__(self, line):
@@ -57,6 +87,34 @@ class Workflow:
             result = rule.evaluate(part)
             if rule.evaluate(part) != "":
                 return result
+
+    def split(self, part_range: PartRange) -> list[PartRange]:
+        new_ranges = []
+        next_range = part_range
+        for rule in self.rules:
+            accepted_range, next_range = rule.split(next_range)
+            new_ranges.append(accepted_range)
+        return new_ranges
+
+
+class RangeAnalyzer:
+    def __init__(self, workflows: list[Workflow]):
+        self._workflows = workflows
+        self._part_range_queue = [PartRange("in", {"x": (1, 4000), "m": (1, 4000),"a": (1, 4000),"s": (1, 4000)})]
+
+    def evaluate(self):
+        n_combinations = 0
+        while self._part_range_queue:
+            part_range = self._part_range_queue.pop(-1)
+            new_ranges = self._workflows[part_range.workflow_id].split(part_range)
+            for new_range in new_ranges:
+                if new_range.workflow_id == "R":
+                    continue
+                elif new_range.workflow_id == "A":
+                    n_combinations += new_range.combinations()
+                else:
+                    self._part_range_queue.append(new_range)
+        return n_combinations
 
 
 def load_data(filename):
@@ -83,7 +141,7 @@ def get_result(workflows, parts):
 
 
 def get_result_2(workflows):
-    pass
+    return RangeAnalyzer(workflows).evaluate()
 
 
 def main():
