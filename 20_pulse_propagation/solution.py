fba1ba7e3df62009714ce7c233d685998332c895
laufinger <l93aufinger@gmail.com>
2024-01-16 20:26:16 +0100

    test: Ensure modules are loaded as correct type

--- 20_pulse_propagation/solution.py
+++ 20_pulse_propagation/solution.py
@@ -1,3 +1,46 @@
+from abc import ABC, abstractmethod
+
+
+class Module(ABC):
+    def __init__(self, destination_modules: list[str]):
+        self._destination_modules = destination_modules
+
+    @abstractmethod
+    def receive(self, pulse: bool):
+        pass
+
+    @abstractmethod
+    def send(self):
+        pass
+
+
+class FlipFlopModule(Module):
+
+    def receive(self, pulse: bool):
+        pass
+
+    def send(self):
+        pass
+
+
+class ConjunctionModule(Module):
+
+    def receive(self, pulse: bool):
+        pass
+
+    def send(self):
+        pass
+
+
+class BroadcasterModule(Module):
+
+    def receive(self, pulse: bool):
+        pass
+
+    def send(self):
+        pass
+
+
 def load_data(filename):
     pass
 
--- 20_pulse_propagation/test.py
+++ 20_pulse_propagation/test.py
@@ -1,15 +1,23 @@
+import pytest
+
 import solution
+from solution import Module, FlipFlopModule, BroadcasterModule, ConjunctionModule
 
 
 class TestPart1:
-    def test_that_data_is_loaded(self):
-        pass
+    def test_that_data_is_loaded_as_a_dict_of_modules(self):
+        modules = solution.load_data("example.txt")
+        assert isinstance(modules["vh"], Module)
+        assert isinstance(modules["vh"], FlipFlopModule)
+        assert isinstance(modules["pb"], ConjunctionModule)
+        assert isinstance(modules["broadcaster"], BroadcasterModule)
 
+    @pytest.mark.skip
     def test_that_result_is_correct_for_example(self):
         data = solution.load_data("example.txt")
         assert solution.get_result(data) == 0
 
-
+@pytest.mark.skip
 class TestPart2:
     def test_that_result_is_correct_for_example(self):
         data = solution.load_data("example.txt")
