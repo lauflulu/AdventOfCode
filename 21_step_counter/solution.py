77f0f3f80eb1d54ff451bead540ddd311fa03353
laufinger <l93aufinger@gmail.com>
2024-01-21 15:22:45 +0100

    feat: Load data

--- 21_step_counter/solution.py
+++ 21_step_counter/solution.py
@@ -3,19 +3,25 @@ import numpy as np
 
 class Gardener:
     def __init__(self, lines: list[str]):
-        self.garden, self.start = self._parse(lines)
+        self.garden, self.start_yx = self._parse(lines)
 
     def _parse(self, lines: list[str]) -> tuple[np.array, tuple]:
-        pass
+        start_yx = (-1, -1)
+        for i, line in enumerate(lines):
+            j = line.find("S")
+            if j > 0:
+                start_yx = (i, j)
+                break
+        return np.array([[col for col in row.strip()] for row in lines]), start_yx
+
 
     def step(self, n=1):
         pass
 
 
-
-
 def load_data(filename) -> Gardener:
-    pass
+    with open(filename, "r") as file:
+        return Gardener(file.readlines())
 
 
 def get_result(gardener: Gardener) -> int:
