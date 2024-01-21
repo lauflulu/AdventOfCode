47625efdd0d9613fcd0721194f88c5cda3f2345d
laufinger <l93aufinger@gmail.com>
2024-01-21 15:15:49 +0100

    test: Ensure data is loaded correctly

--- 21_step_counter/solution.py
+++ 21_step_counter/solution.py
@@ -1,12 +1,28 @@
-def load_data(filename):
+import numpy as np
+
+
+class Gardener:
+    def __init__(self, lines: list[str]):
+        self.garden, self.start = self._parse(lines)
+
+    def _parse(self, lines: list[str]) -> tuple[np.array, tuple]:
+        pass
+
+    def step(self, n=1):
+        pass
+
+
+
+
+def load_data(filename) -> Gardener:
     pass
 
 
-def get_result(data):
+def get_result(gardener: Gardener) -> int:
     pass
 
 
-def get_result_2(data):
+def get_result_2(gardener: Gardener) -> int:
     pass
 
 
--- 21_step_counter/test.py
+++ 21_step_counter/test.py
@@ -1,16 +1,23 @@
+import pytest
+
 import solution
 
 
 class TestPart1:
     def test_that_data_is_loaded(self):
-        pass
+        gardener = solution.load_data("example.txt")
+        assert gardener.garden.shape == (11, 11)
+        assert gardener.start_yx == (5, 5)
 
+    @pytest.mark.skip
     def test_that_result_is_correct_for_example(self):
-        data = solution.load_data("example.txt")
-        assert solution.get_result(data) == 0
+        gardener = solution.load_data("example.txt")
+        gardener.step(6)
+        assert solution.get_result(gardener) == 16
 
 
+@pytest.mark.skip
 class TestPart2:
     def test_that_result_is_correct_for_example(self):
-        data = solution.load_data("example.txt")
-        assert solution.get_result_2(data) == 0
+        gardener = solution.load_data("example.txt")
+        assert solution.get_result_2(gardener) == 0
