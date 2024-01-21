c49f9c911b144eb48963a4bcbc0f972f91811919
laufinger <l93aufinger@gmail.com>
2024-01-21 16:22:42 +0100

    test: Ensure number of reachable tiles is computed

--- 21_step_counter/test.py
+++ 21_step_counter/test.py
@@ -9,11 +9,17 @@ class TestPart1:
         assert gardener.garden.shape == (11, 11)
         assert gardener.start_yx == (5, 5)
 
+    @pytest.mark.parametrize("n_steps, n_tiles", [(1, 2), (2, 4), (3, 6), (6, 16)])
+    def test_that_number_of_reachable_tiles_after_n_steps_is_correct(self, n_steps, n_tiles):
+        gardener = solution.load_data("example.txt")
+        gardener.step(n_steps)
+        assert gardener.number_of_reachable_tiles() == n_tiles
+
     @pytest.mark.skip
     def test_that_result_is_correct_for_example(self):
         gardener = solution.load_data("example.txt")
-        gardener.step(6)
-        assert solution.get_result(gardener) == 16
+        gardener.step(64)
+        assert solution.get_result(gardener) == -1
 
 
 @pytest.mark.skip
