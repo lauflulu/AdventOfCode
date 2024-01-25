17344faa97b6cf0263e2445667440ad8fad81e76
laufinger <l93aufinger@gmail.com>
2024-01-25 19:56:18 +0100

    test: Ensure number of falls is counted

--- 22_sand_slabs/test.py
+++ 22_sand_slabs/test.py
@@ -74,7 +74,6 @@ class TestPart2:
         environment = solution.Environment(blocks)
         assert environment.settle() == 5
 
-    @pytest.mark.skip(reason="Not implemented")
     def test_that_result_is_correct_for_example(self):
         data = solution.load_data("example.txt")
         assert solution.get_result_2(data) == 7
