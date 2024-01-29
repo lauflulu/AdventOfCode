4fc9754dd9fb350681409dc3a9f4a9c55a9b88a8
laufinger <l93aufinger@gmail.com>
2024-01-29 20:47:36 +0100

    test: Ensure data is loaded as matrix

--- 25_snowverload/test.py
+++ 25_snowverload/test.py
@@ -1,15 +1,20 @@
+import pytest
+
 import solution
 
 
 class TestPart1:
-    def test_that_data_is_loaded(self):
-        pass
+    def test_that_data_is_loaded_as_adjacency_matrix(self):
+        graph = solution.load_data("example.txt")
+        assert graph.shape == (15, 15)
 
+    @pytest.mark.skip(reason="Not implemented yet")
     def test_that_result_is_correct_for_example(self):
         data = solution.load_data("example.txt")
-        assert solution.get_result(data) == 0
+        assert solution.get_result(data) == 54
 
 
+@pytest.mark.skip(reason="Not implemented yet")
 class TestPart2:
     def test_that_result_is_correct_for_example(self):
         data = solution.load_data("example.txt")
