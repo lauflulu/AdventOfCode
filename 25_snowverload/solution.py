ef95fb960813ed2cd827a3d04c92c19a3f5b67c0
laufinger <l93aufinger@gmail.com>
2024-01-29 21:03:03 +0100

    refactor: Extract method to parse line

--- 25_snowverload/solution.py
+++ 25_snowverload/solution.py
@@ -5,20 +5,23 @@ def load_data(filename):
     with open(filename, "r") as f:
         nodes = []
         for line in f:
-            node, neighbors = line.split(":")
-            node = node.strip()
-            neighbors = [n.strip() for n in neighbors.strip().split(" ")]
+            neighbors, node = _parse(line)
             for n in [node] + neighbors:
                 if n not in nodes:
                     nodes.append(n)
         adjacency_matrix = np.zeros((len(nodes), len(nodes)))
         for line in open(filename, "r"):
-            node, neighbors = line.split(":")
-            node = node.strip()
-            neighbors = [n.strip() for n in neighbors.strip().split(" ")]
+            neighbors, node = _parse(line)
             for n in neighbors:
                 adjacency_matrix[nodes.index(node), nodes.index(n)] = 1
     return adjacency_matrix + adjacency_matrix.T
+
+
+def _parse(line):
+    node, neighbors = line.split(":")
+    node = node.strip()
+    neighbors = [n.strip() for n in neighbors.strip().split(" ")]
+    return neighbors, node
 
 
 def get_result(data):
