4eb8791b2545961c5a7a45b3baa57b43b31b6070
laufinger <l93aufinger@gmail.com>
2024-01-29 21:01:30 +0100

    test: Ensure matrix properties

--- 25_snowverload/test.py
+++ 25_snowverload/test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 import solution
@@ -7,6 +8,18 @@ class TestPart1:
     def test_that_data_is_loaded_as_adjacency_matrix(self):
         graph = solution.load_data("example.txt")
         assert graph.shape == (15, 15)
+
+    def test_that_diagonal_of_adjacency_matrix_is_zero(self):
+        graph = solution.load_data("example.txt")
+        assert (graph.diagonal() == 0).all()
+
+    def test_that_adjacency_matrix_is_symmetric(self):
+        graph = solution.load_data("example.txt")
+        assert np.all(graph == graph.T)
+
+    def test_that_adjacency_matrix_only_contains_ones_and_zeros(self):
+        graph = solution.load_data("example.txt")
+        assert np.all(np.logical_or(graph == 0, graph == 1))
 
     @pytest.mark.skip(reason="Not implemented yet")
     def test_that_result_is_correct_for_example(self):
