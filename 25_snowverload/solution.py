97ce7ffdb2f137e88f38093568e763c3de349644
laufinger <l93aufinger@gmail.com>
2024-01-29 21:01:14 +0100

    feat: Load graph as matrix

--- 25_snowverload/solution.py
+++ 25_snowverload/solution.py
@@ -1,5 +1,24 @@
+import numpy as np
+
+
 def load_data(filename):
-    pass
+    with open(filename, "r") as f:
+        nodes = []
+        for line in f:
+            node, neighbors = line.split(":")
+            node = node.strip()
+            neighbors = [n.strip() for n in neighbors.strip().split(" ")]
+            for n in [node] + neighbors:
+                if n not in nodes:
+                    nodes.append(n)
+        adjacency_matrix = np.zeros((len(nodes), len(nodes)))
+        for line in open(filename, "r"):
+            node, neighbors = line.split(":")
+            node = node.strip()
+            neighbors = [n.strip() for n in neighbors.strip().split(" ")]
+            for n in neighbors:
+                adjacency_matrix[nodes.index(node), nodes.index(n)] = 1
+    return adjacency_matrix + adjacency_matrix.T
 
 
 def get_result(data):
