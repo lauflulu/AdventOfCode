281b6f3a374331ab38860cb67e1e79d927920202
laufinger <l93aufinger@gmail.com>
2024-01-30 22:08:50 +0100

    feat: Compute result 1 for example data

--- 25_snowverload/solution.py
+++ 25_snowverload/solution.py
@@ -6,29 +6,32 @@ class StoerWagner:
         self.matrix = matrix
         self.index = nodes
         self.nodes = {node: 1 for node in nodes}
+        self.n_nodes = len(nodes)
 
     def get_result(self):
-        return len(self.minimum_cut()[0])
+        minimum_partition = self.minimum_cut()
+        return minimum_partition * (self.n_nodes - minimum_partition)
 
     def minimum_cut(self):
         w_min = np.inf
-        t_min = []
-        while len(self.index) > 1:
+        min_partition = []
+        i = 0
+        while len(self.index) > 2 and i < 10:
+            i += 1
             t, w = self.minimum_cut_phase()
             if w < w_min:
                 w_min = w
-                t_min = t
-        return t_min, w_min
+                min_partition = self.nodes[t]
+        return min_partition
 
     def minimum_cut_phase(self):
         node_a = self.index[0]
         A = [node_a]
-        while A != self.index:
+        while len(A) != len(self.index):
             A.append(self.node_most_tightly_connected_with(A))
         s, t = A[-2], A[-1]
         self.merge_nodes(s, t)
-        print(t)
-        return t, self.sum_of_weights(s)
+        return s, self.sum_of_weights(s)
 
     def node_most_tightly_connected_with(self, A):
         max_connected_node = ""
@@ -52,9 +55,9 @@ class StoerWagner:
         for i in range(len(self.index) - 1):
             self.matrix[i, i] = 0
 
+        self.nodes[s] += self.nodes[t]
         self.index.remove(t)
         self.nodes.pop(t)
-        self.nodes[s] += 1
 
     def sum_of_weights(self, s):
         return sum(self.matrix[self.index.index(s), :])
@@ -87,14 +90,14 @@ def get_result(matrix, nodes):
     return StoerWagner(matrix, nodes).get_result()
 
 
-def get_result_2(data):
+def get_result_2(matrix, nodes):
     pass
 
 
 def main():
-    data = load_data("data.txt")
-    print(get_result(data))
-    print(get_result_2(data))
+    matrix, nodes = load_data("data.txt")
+    print(get_result(matrix, nodes))
+    print(get_result_2(matrix, nodes))
 
 
 if __name__ == '__main__':
