b115bdee66bee0d935cba7fc67a9f5cdf4c45727
laufinger <l93aufinger@gmail.com>
2024-01-30 21:22:18 +0100

    refactor: Lay out Stoer-Wagner minimum-cut algorithm

--- 25_snowverload/solution.py
+++ 25_snowverload/solution.py
@@ -4,10 +4,58 @@ import numpy as np
 class StoerWagner:
     def __init__(self, matrix, nodes):
         self.matrix = matrix
-        self.nodes = nodes
+        self._index = nodes
+        self.nodes = {node: 1 for node in nodes}
 
-    def get_min_cut(self):
-        pass
+    def get_result(self):
+        return len(self.minimum_cut()[0])
+
+    def minimum_cut(self):
+        w_min = np.inf
+        t_min = []
+        while len(self._index) > 1:
+            t, w = self.minimum_cut_phase()
+            if w < w_min:
+                w_min = w
+                t_min = t
+        return t_min, w_min
+
+    def minimum_cut_phase(self):
+        node_a = self._index[0]
+        A = [node_a]
+        while A != self._index:
+            A.append(self._node_most_tightly_connected_with(A))
+        s, t = A[-2], A[-1]
+        self._merge_nodes(s, t)  # matrix and nodes
+        return t, self._sum_of_weights(t)
+
+    def _node_most_tightly_connected_with(self, A):
+        max_connected_node = ""
+        max_weight = 0
+        for node in self._index:
+            if node not in A:
+                node_weight = self._weight(node, A)
+                if node_weight > max_weight:
+                    max_weight = node_weight
+                    max_connected_node = node
+        return max_connected_node
+
+    def _weight(self, node, A):
+        return sum(self.matrix[self._index.index(node), self._index.index(a)] for a in A)
+
+    def _merge_nodes(self, s, t):
+        self._index.remove(t)
+        self.nodes.pop(t)
+        self.nodes[s] += 1
+        self.matrix[self._index.index(s), :] += self.matrix[self._index.index(t), :]
+        self.matrix[:, self._index.index(s)] += self.matrix[:, self._index.index(t)]
+        self.matrix = np.delete(self.matrix, self._index.index(t), 0)
+        self.matrix = np.delete(self.matrix, self._index.index(t), 1)
+        for i in range(len(self._index)):
+            self.matrix[i, i] = 0
+
+    def _sum_of_weights(self, t):
+        return sum(self.matrix[self._index.index(t), :])
 
 
 def load_data(filename) -> tuple[np.ndarray, list[str]]:
@@ -33,8 +81,8 @@ def _parse(line):
     return neighbors, node
 
 
-def get_result(data):
-    pass
+def get_result(matrix, nodes):
+    return StoerWagner(matrix, nodes).get_result()
 
 
 def get_result_2(data):
