c62e0fd7e5a65863a4836f18567918a953b0a9a8
laufinger <l93aufinger@gmail.com>
2024-06-10 21:59:10 +0200

    refactor: Remove unnecessary sleep

--- 2022/02_rock_paper_scissors/input_script.py
+++ 2022/02_rock_paper_scissors/input_script.py
@@ -1,5 +1,4 @@
 import serial
-import time
 
 ser = serial.Serial("COM3", 9600)
 
@@ -7,5 +6,4 @@ with open("input.txt") as file:
     for line in file:
         message = f"{line.strip()}\r\n".encode()
         ser.write(message)
-        time.sleep(0.01)
         print(f"Sent message: {message}, response: {ser.readline()}")
