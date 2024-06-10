af1cf50dee95152d7b0b429b584ba9ffc946bdc2
laufinger <l93aufinger@gmail.com>
2024-06-10 21:58:01 +0200

    feat: Compute result 1

--- 2022/02_rock_paper_scissors/src/main.cpp
+++ 2022/02_rock_paper_scissors/src/main.cpp
@@ -1,18 +1,14 @@
-#include <Arduino.h>
+#include <main.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+SerialAdapter serial_adapter{Serial};
+Score score{serial_adapter};
+void setup()
+{
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  score.poll();
+  Serial.println(String(score.get_total_score()));
 }
