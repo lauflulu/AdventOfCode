8d64f38bd9bb7e7235b6b2225a0d469f34d9e5e9
laufinger <l93aufinger@gmail.com>
2024-06-10 21:17:06 +0200

    feat: Look up score for every possible game

--- 2022/02_rock_paper_scissors/lib/Score/Score.cpp
+++ 2022/02_rock_paper_scissors/lib/Score/Score.cpp
@@ -2,10 +2,46 @@
 
 uint32_t Score::get_total_score()
 {
-    return 0;
+    return score;
 }
 
 void Score::poll(void)
 {
     String message = serial.read_line();
+    if (message.startsWith("A X"))
+    {
+        score += 4;
+    }
+    else if (message.startsWith("A Y"))
+    {
+        score += 8;
+    }
+    else if (message.startsWith("A Z"))
+    {
+        score += 3;
+    }
+    else if (message.startsWith("B X"))
+    {
+        score += 1;
+    }
+    else if (message.startsWith("B Y"))
+    {
+        score += 5;
+    }
+    else if (message.startsWith("B Z"))
+    {
+        score += 9;
+    }
+    else if (message.startsWith("C X"))
+    {
+        score += 7;
+    }
+    else if (message.startsWith("C Y"))
+    {
+        score += 2;
+    }
+    else if (message.startsWith("C Z"))
+    {
+        score += 6;
+    }
 }
--- 2022/02_rock_paper_scissors/test/test_score.cpp
+++ 2022/02_rock_paper_scissors/test/test_score.cpp
@@ -18,3 +18,84 @@ TEST_F(TestScore, WhenInitializedIsZero)
 {
     ASSERT_EQ(score->get_total_score(), 0);
 }
+
+TEST_F(TestScore, WhenAXShouldBe4)
+{
+    mock_serial->set_input("A X\r\n");
+
+    score->poll();
+
+    ASSERT_EQ(score->get_total_score(), 4);
+}
+
+TEST_F(TestScore, WhenAYShouldBe8)
+{
+    mock_serial->set_input("A Y\r\n");
+
+    score->poll();
+
+    ASSERT_EQ(score->get_total_score(), 8);
+}
+
+TEST_F(TestScore, WhenAZShouldBe3)
+{
+    mock_serial->set_input("A Z\r\n");
+
+    score->poll();
+
+    ASSERT_EQ(score->get_total_score(), 3);
+}
+
+TEST_F(TestScore, WhenBXShouldBe1)
+{
+    mock_serial->set_input("B X\r\n");
+
+    score->poll();
+
+    ASSERT_EQ(score->get_total_score(), 1);
+}
+
+TEST_F(TestScore, WhenBYShouldBe5)
+{
+    mock_serial->set_input("B Y\r\n");
+
+    score->poll();
+
+    ASSERT_EQ(score->get_total_score(), 5);
+}
+
+TEST_F(TestScore, WhenBZShouldBe9)
+{
+    mock_serial->set_input("B Z\r\n");
+
+    score->poll();
+
+    ASSERT_EQ(score->get_total_score(), 9);
+}
+
+TEST_F(TestScore, WhenCXShouldBe7)
+{
+    mock_serial->set_input("C X\r\n");
+
+    score->poll();
+
+    ASSERT_EQ(score->get_total_score(), 7);
+}
+
+TEST_F(TestScore, WhenCYShouldBe2)
+{
+    mock_serial->set_input("C Y\r\n");
+
+    score->poll();
+
+    ASSERT_EQ(score->get_total_score(), 2);
+}
+
+TEST_F(TestScore, WhenCZShouldBe6)
+{
+    mock_serial->set_input("C Z\r\n");
+
+    score->poll();
+
+    ASSERT_EQ(score->get_total_score(), 6);
+}
