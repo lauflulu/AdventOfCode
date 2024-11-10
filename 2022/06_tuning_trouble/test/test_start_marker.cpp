ea4ca36f43c52c8b00999982413dc421e8d4cd50
laufinger <l93aufinger@gmail.com>
2024-11-10 19:04:38 +0100

    test: Ensure remaining examples are computed correctly

--- 2022/06_tuning_trouble/test/test_start_marker.cpp
+++ 2022/06_tuning_trouble/test/test_start_marker.cpp
@@ -19,7 +19,7 @@ TEST(MessageWithoutStartMarker, FindStartMarkerShouldReturn0)
     ASSERT_EQ(marker_position, 0);
 }
 
-TEST(MessageOnlyWithStartMarker, FindStartMarkerShouldReturn0)
+TEST(MessageOnlyWithStartMarker, FindStartMarkerShouldBeAtPosition4)
 {
     String message = "abcdefghijklm";
 
@@ -35,4 +35,40 @@ TEST(ExampleMessage, StartMarkerShouldBeAtPosition7)
     uint32_t marker_position = find_start_marker(message);
 
     ASSERT_EQ(marker_position, 7);
+}
+
+TEST(ExampleMessage2, StartMarkerShouldBeAtPosition5)
+{
+    String message = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+
+    uint32_t marker_position = find_start_marker(message);
+
+    ASSERT_EQ(marker_position, 5);
+}
+
+TEST(ExampleMessage3, StartMarkerShouldBeAtPosition6)
+{
+    String message = "nppdvjthqldpwncqszvftbrmjlhg";
+
+    uint32_t marker_position = find_start_marker(message);
+
+    ASSERT_EQ(marker_position, 6);
+}
+
+TEST(ExampleMessage4, StartMarkerShouldBeAtPosition10)
+{
+    String message = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+
+    uint32_t marker_position = find_start_marker(message);
+
+    ASSERT_EQ(marker_position, 10);
+}
+
+TEST(ExampleMessage5, StartMarkerShouldBeAtPosition11)
+{
+    String message = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+
+    uint32_t marker_position = find_start_marker(message);
+
+    ASSERT_EQ(marker_position, 11);
 }
